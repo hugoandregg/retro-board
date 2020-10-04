2b178d6212aa166c93ac331e596bd01908a441df
Hugo André <hugoandregg@gmail.com>
2020-10-04 16:19:58 -0300

    Adding task list

---
+++ retro-board-ui/src/initial-data.js
@@ -0,0 +1,18 @@
+const initialData = {
+  tasks: {
+    "task-1": { id: "task-1", content: "Take out the garbage" },
+    "task-2": { id: "task-2", content: "Charge my phone" },
+    "task-3": { id: "task-3", content: "Watch Series" },
+    "task-4": { id: "task-4", content: "Cook Dinner" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To Do",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+    },
+  },
+  columnOrder: ["column-1"],
+};
+
+export default initialData;
