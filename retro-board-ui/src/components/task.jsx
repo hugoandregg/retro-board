47b4e7d364bec2b985a088e66d50bb3a05b6bb71
Hugo André <hugoandregg@gmail.com>
2020-10-04 16:57:11 -0300

    Making columns and tasks droppable and draggable

--- retro-board-ui/src/components/task.jsx
+++ retro-board-ui/src/components/task.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
+  background-color: white;
 `;
 
 const Task = (props) => {
-  return <Container>{props.task.content}</Container>;
+  return (
+    <Draggable draggableId={props.task.id} index={props.index}>
+      {(provided) => (
+        <Container
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {props.task.content}
+        </Container>
+      )}
+    </Draggable>
+  );
 };
 
 export default Task;
