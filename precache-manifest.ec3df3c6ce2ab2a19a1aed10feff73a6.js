ae4cf88fb43cd356efd0d0d2cceebcc0175094e3
hugoandregg <hugoandregg@gmail.com>
2020-10-13 12:54:05 +0000

    Deploying to gh-pages from  @ 26c9bda123407a923c80f7dd800842588e36ef58 🚀

---
+++ precache-manifest.ec3df3c6ce2ab2a19a1aed10feff73a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b398fecc385ecd90cdc635f5fa0a0511",
+    "url": "/retro-board/index.html"
+  },
+  {
+    "revision": "a885e9f61fa1da18e67b",
+    "url": "/retro-board/static/css/2.8da32990.chunk.css"
+  },
+  {
+    "revision": "a885e9f61fa1da18e67b",
+    "url": "/retro-board/static/js/2.aa286ddd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/retro-board/static/js/2.aa286ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfab8f73d5f0f6136763",
+    "url": "/retro-board/static/js/main.89a41a1f.chunk.js"
+  },
+  {
+    "revision": "9f9a5f537f7791c731b9",
+    "url": "/retro-board/static/js/runtime-main.cecaccae.js"
+  }
+]);
